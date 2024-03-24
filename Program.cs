0ddf9d17f582f63aa05f80cb2a9d35b3aeced3bd
Reinhard Situmeang <101586289+vankishy@users.noreply.github.com>
2024-03-24 20:18:27 +0700

    menambahkan class SayaTubeVideo

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = null;
+
+        try
+        {
+            video = new SayaTubeVideo("Tutorial Design by Contract - Reinhard Situmeang");
+            for (int i = 0; i < 10000000; i++)
+            {
+                video.IncreasePlayCount(1);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+        finally
+        {
+            if (video != null)
+            {
+                video.PrintVideoDetails();
+            }
+        }
+    }
+}
---
+++ SayaTubeVideo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.id = RandomNumber();
+            this.title = title;
+            this.playCount = 0;
+        }
+
+        private int RandomNumber()
+        {
+            Random random = new Random();
+            return random.Next(00000, 99999);
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine($"ID : {id}");
+            Console.WriteLine($"Title : {title}");
+            Console.WriteLine($"Play Count : {playCount}");
+
+        }
+}
